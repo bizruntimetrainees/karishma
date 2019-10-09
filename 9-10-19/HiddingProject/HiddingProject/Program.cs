85f82f31b706356d5beb049bb7cdb7cf26a740f3
karishma <shaik.k@bizruntime.net>
2019-10-09 18:27:40 +0530

    programs

---
+++ 9-10-19/HiddingProject/HiddingProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddingProject
+{
+    class Hidding
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("show parent method is called");
+        }
+        public void Test()
+        {
+            Console.WriteLine(" parent test method is called");
+        }
+    }
+    class Child : Hidding
+    {
+        public override void Show()
+        {
+            Console.WriteLine("show child method is called");
+      }
+        public void ParentShow()
+        {
+            base.Show();
+        }
+        public void ParentTest()
+        {
+            base.Test();
+        }
+        public new void Test()
+      {
+         Console.WriteLine("  child test method is called");
+       }
+
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            c.ParentShow();
+            c.ParentTest();
+            c.Show();
+            c.Test();
+            Hidding h = c;
+            h.Show();
+            h.Test();
+            Console.ReadLine();
+        }
+    }
+}
