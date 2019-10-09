67d2f1d4b227c456bb2756e3f1593a99da9d2092
karishma <shaik.k@bizruntime.net>
2019-10-09 19:32:50 +0530

    programs

---
+++ 9-10-19/OverlodingProject/OverlodingProject/Overload.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverlodingProject
+{
+    class OverLoad
+    {
+        public void Test()
+        {
+            Console.WriteLine("method called");
+        }
+        public void Test1(int i)
+        {
+            Console.WriteLine("method1 called " + i);
+        }
+        public void Test2(string s)
+        {
+            Console.WriteLine("method2 called " + s);
+        }
+        public void Test3(int i,string s)
+        {
+            Console.WriteLine("method3 called " + i + " " + s);
+        }
+        public void Test4(string s,int i)
+        {
+            Console.WriteLine("method4 called "+s+" "+i);
+        }
+        static void Main(string[] args)
+        {
+            OverLoad o = new OverLoad();
+            o.Test();
+            o.Test1(10);
+            o.Test2("hi");
+            o.Test3(10,"hi");
+            o.Test4("hi",10);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9-10-19/OverriddenProject/OverriddenProject/override.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverriddenProject
+{
+    class Override
+    {
+        public void Show()
+        {
+            Console.WriteLine(" parent show method is called");
+        }
+        public virtual void Test()
+        {
+            Console.WriteLine(" Test method is called");
+        }
+    } 
+    class Child : Override
+    {
+        public void Show(int i)
+        {
+            Console.WriteLine("child show method is called");
+        }
+        public override void Test()
+        {
+            Console.WriteLine(" child Test method is called");
+        }
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            c.Show();
+            c.Show(10);
+            c.Test();
+            Console.ReadLine();
+
+        }
+    }
+}
