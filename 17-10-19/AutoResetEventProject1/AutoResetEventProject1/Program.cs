382d8a653628408a3cb3f7c8e72fb5fbff51abdd
karishma <shaik.k@bizruntime.net>
2019-10-17 19:01:29 +0530

    AutoResetEvent

---
+++ 17-10-19/AutoResetEventProject1/AutoResetEventProject1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEventProject1
+{
+    class Program
+    {
+        static AutoResetEvent obj = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            new Thread(Method).Start();
+              Console.ReadLine();
+            obj.Set();
+            Console.ReadLine();
+            obj.Set();
+           
+        }
+        public static void Method()
+        {
+            Console.WriteLine("starting..1..");
+            obj.WaitOne();
+            Console.WriteLine("finishing..1...");
+            Console.WriteLine("starting..2..");
+            obj.WaitOne();
+            Console.WriteLine("finishing..2..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 17-10-19/ManualResetEvent/ManualResetEvent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManualResetEvent1
+{
+    class Program
+    {
+        static ManualResetEvent obj = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            new Thread(Method).Start();
+            Console.ReadLine();
+            obj.Set();
+           
+
+
+
+        }
+        public static void Method()
+        {
+            Console.WriteLine("starting..1..");
+            obj.WaitOne();
+            Console.WriteLine("finishing..1...");
+            Console.WriteLine("starting..2..");
+            obj.WaitOne();
+            Console.ReadLine();
+            Console.WriteLine("finishing..2..");
+            Console.ReadKey();
+        }
+    }
+}
