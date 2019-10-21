6cf9c1c4748ea215505d92d4be9158cc803c550b
karishma <shaik.k@bizruntime.net>
2019-10-21 20:19:36 +0530

    scoket

---
+++ 21-10-19/ClientProject/ClientProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+
+using System.Threading.Tasks;
+
+namespace ClientProject
+{
+    class Program
+    {
+        static Socket sck;
+
+       
+
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11700);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch
+            {
+                Console.WriteLine("connection faild");
+                Main(args);
+            }
+            Console.WriteLine("enter text");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(text);
+
+            sck.Send(data);
+            Console.WriteLine("data sent \r\n");
+            Console.WriteLine("press key to continue");
+            Console.ReadKey();
+            sck.Close();
+        }
+    }
+}
---
+++ 21-10-19/ClientProject/Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SocketProject
+{
+    class Program
+    {
+        static byte[] Buffer { get; set; }
+        static Socket sck;
+        static void Main(string[] args)
+        {
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(0, 1234));
+            sck.Listen(100);
+            Socket accepted = sck.Accept();
+            Buffer = new byte[accepted.SendBufferSize];
+            int bytesRead = accepted.Receive(Buffer);
+            byte[] formatted = new byte[bytesRead];
+            for (int i = 0; i < bytesRead; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strdata = Encoding.ASCII.GetString(formatted);
+            Console.Write(strdata + "\r\n");
+            Console.ReadKey();
+            sck.Close();
+            accepted.Close();
+        }
+    }
+}
