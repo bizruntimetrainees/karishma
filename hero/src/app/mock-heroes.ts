d1cb8fe7ce37e4aeca37ac29e0b79e39188d9725
karishma <shaik.k@bizruntime.net>
2019-11-06 21:49:54 +0530

    angular

---
+++ hero/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Raj' },
+  { id: 2, name: 'Kumar' },
+  { id: 3, name: 'Bella' },
+  { id: 4, name: 'Cristy' },
+  { id: 5, name: 'heav' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Tornado' }
+];
